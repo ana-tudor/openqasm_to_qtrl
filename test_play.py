0949a5b70b022a9fee9333ab4c9cadb7504dd553
Ana Tudor <anamtudor@berkeley.edu>
2019-07-01 13:00:19 -0700

    to view files

--- test_play.py
+++ test_play.py
@@ -1,5 +1,18 @@
 import os, sys
 import numpy as np
+sys.path.append("/home/wlav/quantum/Gang/qtrl/")
+import pkgutil
+import qtrl
+package = qtrl
+for importer, modname, ispkg in pkgutil.iter_modules(package.__path__):
+    print("Found submodule {} (is a package: {})".format(modname, ispkg))
+
+#import qtrl.sequencer
+#import importlib.util
+#spec = importlib.util.spec_from_file_location("qtrl.sequencer", "/home/wlav/quantum/Gang/qtrl/qtrl/sequencer/sequencer.py")
+#sequencer = importlib.util.module_from_spec(spec)
+#spec.loader.exec_module(sequencer)
+
 
 def verify_output(name):
     with open(os.path.join('/home/wlav/quantum/Gang/qtrl/tests/ref_outputs','{}_output.np'.format(name)), 'rb') as f:
@@ -12,7 +25,7 @@ def test_arb_seq(file_name):
     ns = 1e-9
 
     import logging
-    caplog.set_level(logging.DEBUG, logger='qtrl')
+    #caplog.set_level(logging.DEBUG, logger='qtrl')
 
     from qtrl.sequencer import Sequence
 
@@ -26,7 +39,7 @@ def test_arb_seq(file_name):
     for line in l:
         s_ref, e_ref = seq.append([self.cfg.pulses[line]], element=0, end_delay=10*ns)
     
-    self.cfg.add_readout(self.cfg, seq=seq, herald_refs=['Start', 'Start'], readout_refs=['Start', e_ref])
+    #self.cfg.add_readout(self.cfg, seq=seq, herald_refs=['Start', 'Start'], readout_refs=['Start', e_ref])
     seq.compile()
 
     assert seq.array.shape == (12, 2, 122110, 1)
@@ -36,7 +49,7 @@ def test_arb_seq(file_name):
         if val.any():
             active_channels += 1
 
-    active_channels == 5 #2 for qubit, 3 for readout
+    assert active_channels == 5 #2 for qubit, 3 for readout
 
 
 
@@ -50,5 +63,5 @@ list_names = ['interleaved_coherence',\
               'rabi']
 
 #for name in list_names:
-verify_output(list_names[3])
-
+#verify_output(list_names[3])
+test_arb_seq(sys.argv[1])
